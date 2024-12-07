f3232fd78597451fab17d4bd08d0562797523ddc
Ahmad Radi <ahmadradi11@gmail.com>
2024-12-07 23:24:24 +0900

    delete secret

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-var CHANNEL_ACCESS_TOKEN = "bWFiJDXpUw/npxoDw4pvjGI5vi5HElRsBY7LjMXwp+2uYc2mgYHLGW7xznIJzOlithl7ESENYLHZkRkmeaDVrzpEPpJovB+r0nKHL1askH37j4bSNQVgQYAViscw2g7KmrhnqcNaraQbqsmvn6OgFQdB04t89/1O/w1cDnyilFU=";
-var VISIONAPI_KEY = 'AIzaSyAf87ZJeliRw-CCGjry7Cf9LUldd1lOEq8';
+var CHANNEL_ACCESS_TOKEN = "";
+var VISIONAPI_KEY = '';
 
 function toSS(data){
-  SpreadsheetApp.openById("1Wmr6ngxg9-zvlay04JdjEtA4YCpArX5w2upJSs25Aug").getSheetByName("logs").appendRow([data]); 
+  SpreadsheetApp.openById("").getSheetByName("logs").appendRow([data]); 
 }
